e8748c410fec378733dbfae36a527e801280a488
Adi Mukund <adityamukund@gmail.com>
2022-06-27 11:20:00 -0700

    Fig 4 act vs rep scatter fix

--- fig_4/02_avr_scatter/avr_scatter.R
+++ fig_4/02_avr_scatter/avr_scatter.R
@@ -45,30 +45,42 @@ d5_color = "#28bedb"
 d2_color = "#dbc60d"
 
 concats = readr::read_csv("../../fig_2/01_activators_synergy/pairs_baselinesums.csv")
+
+concats %>%
+    dplyr::filter(composition %in% c("A-R", "R-A", "A-N", "N-A", "R-N", "N-A", "C-C")) %>%
+    dplyr::mutate(ctype = case_when(
+        composition %in% c("A-R", "R-A") ~ "Both",
+        composition %in% c("A-N", "N-A") ~ "Act",
+        composition %in% c("R-N", "N-A") ~ "Rep",
+        composition == "C-C" ~ "Control"
+    )) -> cdf
+
 p = ggplot()
 p = p + geom_point(
-    data = dplyr::filter(concats, character %in% c("Control", "Both")),
+    data = cdf,
     aes(
         x = avg_enrichment_d2,
         y = avg_enrichment_d5,
-        color = character,
+        color = ctype,
     ),
     size = 0.15
 )
-p = p + geom_hline(yintercept = d5_threshold, color = 'black', linetype = 'dashed')
+p = p + geom_hline(yintercept = d5_threshold, color = 'black', linetype = 'dashed') 
 p = p + geom_vline(xintercept = d2_threshold, color = 'black', linetype = 'dashed')
 p = p + scale_color_manual(
     name = "",
-    breaks = c("Both", "Control"),
+    breaks = c("Both", "Act", "Rep", "Control"),
     labels = c("A-R",
+               "A-N/A",
+               "R-N/A",
                "Control"),
-    values = c("#0fa616", '#777777'))
-p = p + coord_fixed(xlim = c(-5.5, 5.5), ylim = c(-5.5, 5.5))
+    values = c("#0fa616", d2_color, d5_color, '#777777'))
+p = p + coord_fixed(xlim = c(-5.5, 5.5), ylim = c(-7, 4.5))
 p = p + guides(color = guide_legend(override.aes = list(size = 3)))
 p = p + labs(x = bquote("Activation" ~ log[2] ~ "(ON:OFF)"),
              y = bquote("Repression" ~ log[2] ~ "(ON:OFF)"))
 p = p + theme_bw() 
-p = p + theme(legend.position = c(0.8, 0.125), legend.title = element_blank(), 
+p = p + theme(legend.position = c(0.8, 0.2), legend.title = element_blank(), 
               panel.grid = element_blank(), legend.margin = margin(unit(c(0, 0, 0, 0), units = "mm"),),
               legend.key.size = unit(c(4), units = "mm"))
 ggsave("./d2_vs_d5_scatter.pdf", p, height = 2.5, width = 2.5)
